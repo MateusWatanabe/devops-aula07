1f32026ce3562231ca052b8c4f8e120bc5c1100d
MateusWatanabe <30847248+MateusWatanabe@users.noreply.github.com>
2017-09-26 21:14:20 -0300

    Update testes_inic.py

--- src/testes_inic.py
+++ src/testes_inic.py
@@ -4,17 +4,17 @@ erroInicializar = False
 jogovelha.inicializar()
 jogo = jogovelha.tabuleiro()
 if len(jogo) != 3:
-erroInicializar = True
+  erroInicializar = True
 else:
-for linha in jogo:
+  for linha in jogo:
 if len(linha) != 3:
-erroInicializar = True
+  erroInicializar = True
 else:
 for elemento in linha:
-if elemento != '.':
-erroInicializar = True
-if erroInicializar:
-print('Erro!')
-sys.exit(1)
-else:
-sys.exit(0)
+  if elemento != '.':
+    erroInicializar = True
+  if erroInicializar:
+    print('Erro!')
+    sys.exit(1)
+  else:
+  sys.exit(0)
